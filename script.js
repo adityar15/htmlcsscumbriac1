4972c8fb70b6af347776cc675a76cee247a6722b
Aditya Kadam <adityakadam@Adityas-MacBook-Air.local>
2023-10-17 20:02:08 +0100

    js practical session

--- script.js
+++ script.js
@@ -1,136 +1,52 @@
-// console.log('Hello World')
-
-// let firstname
-
-
-
-// firstname = "Adi"
-
-// const tax = 40
-// const vat = 40
-
-// // let total = vat / tax
-
-// // console.log(total)
-
-// let total = "197"
-// let firstTimeUser = true
-
-// let canHaveDiscount = total == 200 || firstTimeUser
-// console.log(canHaveDiscount)
-
-// let isEqual = total == 197
-
-// let provideDiscount = firstTimeUser != true
-
-// let statement1 = "This is JS session"
-// let statement2 = "This is after break"
-
-// console.log(statement1 + " " + statement2)
-
-
-// function calculate(num1, num2) {
-//     let sum = num1 + num2 
-//     let sub = num1 - num2
-//     let mul = num1 * num2
-//     let div = num1 / num2
-
-//     return sum + sub + mul + div
-// }
-
-// console.log(calculate(10, 20))
-// console.log(calculate(55, 65))
-
-
-// let names = ["Aditya", "Adi", {
-//     name: "Aditya"
-// }]
-
-// let person = {
-//     name: "Aditya",
-//     profession: "Self Employed"
-// }
-
-// for(let i = 0; i < names.length; i++)
-// {
-// //    alkdajsdkl
-// }
-
-
-// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-
-// const evenNumbers = numbers.filter(
-//     (number) => {
-//         return number % 2 == 0
-//     }
-// )
-
-// console.log(evenNumbers)
-
-
-// const squares = evenNumbers.map(
-//     (number)=>{
-//         return number * number
-//     }
-// )
-
-// console.log(squares)
-
-
-window.addEventListener('load', (event)=>{
-    const h1 = document.getElementById("main-title")
-    h1.innerHTML = "Changed using JS"
-
-    const button = document.querySelector("#btn")
-
-    button.addEventListener('click', (event)=>{
-        h1.innerHTML = "Welcome back"
-        h1.style.color = "blue"
-        setTimeout(() => {
-            h1.innerHTML = "Changed using JS"
-        }, 2000)
-
-    })
-
-
-    const button2 = document.querySelector("#new-ele")
-
-    button2.addEventListener('click', (event)=>{
-        const newElement = document.createElement("a")
-        newElement.innerHTML = "New Link"
-        newElement.href = "https://google.com"
-        document.body.appendChild(newElement)
-    })
-
-    const form = document.querySelector("#contact-form")
-
-    form.addEventListener("submit", (event)=>{
-        event.preventDefault()
-
-        const formData = new FormData(form)
-
-        const data = Object.fromEntries(formData.entries())
-
-        console.log("formData", formData)
-        console.log("data", data)
-    })
-
+window.addEventListener('load', ()=>{
+    const form = document.querySelector("#search-form")
+    form.addEventListener('submit', getUserDetails)
 })
 
+function getUserDetails(event)
+{
+    event.preventDefault()
+    
+    const formData = new FormData(event.target)
+    const data = Object.fromEntries(formData.entries())
 
-window.addEventListener('load', (event) =>{
-    const form = document.querySelector("#todo-form")
+    if(data.username == "" || !data.username)
+    {
+        alert("Please enter a valid username")
+        return
+    }
+  
 
-    const list = document.querySelector("#list-items")
 
-    form.addEventListener('submit', (event) => {
-        event.preventDefault()
-        const formData = new FormData(form)
-        const data = Object.fromEntries(formData.entries())
+    fetch(`https://api.github.com/users/${data.username}`)
+    .then(response => response.json())
+    .then(apiData => {
+        // console.log("entered this section")
 
-        const li = document.createElement("li")
-        li.innerHTML = data.todo
-        list.appendChild(li)
-    }) 
-})
+        if(apiData.message && apiData.message == "Not Found")
+        {
+            alert("Enter a valid username because this one was not found")
+            return
+        }
+
+        const image = document.querySelector("#image")
+        const h3 = document.querySelector("#name")
+
+        const bioSpan = document.querySelector("#bio")
+        const resposSpan = document.querySelector("#public_repos")
+
+        image.setAttribute("src", apiData.avatar_url)
+
+        h3.innerHTML = apiData.name
+        bioSpan.innerHTML = apiData.bio
+        resposSpan.innerHTML = apiData.public_repos
+
+        console.log("data", apiData)
+    })
+
+   
+    
+}
+
+
+
